8236c32f62407ce33a62343b8bf8e62296bc3473
tudor baraian <baraiantudor@yahoo.com>
2024-04-25 00:21:36 +0300

    maybe final

--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -6,33 +6,61 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
-public class Client {
-    public static void main(String[] args) {
-        Socket socket;
-        DataOutputStream out;
-        BufferedReader in;
-        BufferedReader c;
-        c = new BufferedReader(new InputStreamReader(System.in));
+public class Client extends Thread{
+    private final String ip;
+    private final int port;
+    private final int id;
+    public volatile boolean running = true;
 
-        String line;
+    Socket socket;
+    DataOutputStream out;
+    BufferedReader in;
 
+    public Client(String ip,int port,int id){
+        this.ip = ip;
+        this.port = port;
+        this.id = id;
+    }
+
+    public void stopRunning(){
+        running = false;
+        try {
+            if (socket != null && !socket.isClosed()) {
+                socket.close();
+            }
+        } catch (IOException ignored) {
+        }
+    }
+
+    public void run(){
         try {
             System.out.println("\nClient online!");
-            socket = new Socket(args[0], Integer.parseInt(args[1]));
+            socket = new Socket(ip, port);
             System.out.println("Socket info from client: " + socket);
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             out = new DataOutputStream(socket.getOutputStream());
-            while (true) {
-                System.out.print("\nWrite your message here:");
-                line = c.readLine();
-                out.writeBytes(line + "\n");
-                out.flush();
 
-                System.out.println("Sent to the server:" + line);
-                line = in.readLine();
-                System.out.println("Reception from the server:" + line);
+            while (!Thread.currentThread().isInterrupted() && running) {
+                int number = in.read();
+                if(number%(1+id*2)!=0)continue;
+                System.out.println("Client: " + id + " received:" + number);
+                out.writeBytes("ACK\n");
+                out.flush();
             }
         } catch (IOException ignored) {
+        } finally {
+            try {
+                if (out != null) {
+                    out.close();
+                }
+                if (in != null) {
+                    in.close();
+                }
+                if (socket != null) {
+                    socket.close();
+                }
+            } catch (IOException ignored) {
+            }
         }
     }
 
--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -6,31 +6,63 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.List;
+import java.util.Random;
 
 public class Server {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         ServerSocket serverSocket = null;
-        boolean listening = true;
-
+        int port = 5565;
         try {
-            serverSocket = new ServerSocket(Integer.parseInt(args[0]));
+            serverSocket = new ServerSocket(port);
         } catch (IOException e) {
-            System.err.println("Error on the port:" + Integer.parseInt(args[0]));
+            System.err.println("Error on the port:" + port);
             System.exit(-1);
         }
+        System.out.println("Server started");
+        Socket socket1,socket2;
+        Client client1,client2;
+        Worker worker1,worker2;
+        client1 = new Client("127.0.0.2",port, 1);
+        client1.start();
 
-        while (listening)
-            new Worker(serverSocket.accept()).start();
-        serverSocket.close();
+        socket1 = serverSocket.accept();
+        worker1 = new Worker(socket1);
+        worker1.start();
 
+        client2 = new Client("127.0.0.3",port, 2);
+        client2.start();
+
+        socket2 = serverSocket.accept();
+        worker2 = new Worker(socket2);
+        worker2.start();
+
+        DataOutputStream out1 = new DataOutputStream(socket1.getOutputStream());
+        DataOutputStream out2 = new DataOutputStream(socket2.getOutputStream());
+        DataOutputStream []outputStreams = {out1,out2};
+        Random random = new Random();
+        for(int index = 0 ;index<=100;index++){
+            Thread.sleep(50);
+            int cur = random.nextInt(2);
+            System.out.println("Current number: " + index);
+            outputStreams[cur].write(index);
+            outputStreams[cur].flush();
+        }
+
+        client1.stopRunning();
+        client1.interrupt();
+        client2.stopRunning();
+        client2.interrupt();
+        worker1.stopRunning();
+        worker1.interrupt();
+        worker2.stopRunning();
+        worker2.interrupt();
     }
 }
 class Worker extends Thread {
     private final Socket socket;
-
-    DataOutputStream out = null;
+    public volatile boolean running = true;
     BufferedReader in = null;
-    boolean flag;
     String line;
 
     public Worker(Socket socket) {
@@ -39,26 +71,37 @@ class Worker extends Thread {
         System.out.println("\nSocket info from server: " + socket);
     }
 
-    public void run() {
-
+    public void stopRunning(){
+        running = false;
         try {
-            in = new BufferedReader(
-                    new InputStreamReader(socket.getInputStream()));
-            out = new DataOutputStream(socket.getOutputStream());
-            System.out.println("Client acceptat!/Accepted client!\n");
-            flag = true;
-            while (flag) {
-                line = in.readLine();
-                System.out.println("Primit de la client/Received from the client:" + line + "\n");
-                flag = (line.compareTo("bye") != 0);
-                out.writeBytes(line + "\n");
+            if (socket != null && !socket.isClosed()) {
+                socket.close();
             }
+        } catch (IOException ignored) {
+        }
+    }
 
-            out.close();
-            in.close();
-            socket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+    public void run() {
+        try {
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            System.out.println("Client accepted!");
+
+            while (running && !Thread.currentThread().isInterrupted()) {
+                line = in.readLine();
+                if (line == null) break;
+                System.out.println("Received from client: " + line);
+            }
+        } catch (IOException ignored) {
+        } finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+                if (socket != null) {
+                    socket.close();
+                }
+            } catch (IOException ignored) {
+            }
         }
     }
 }
